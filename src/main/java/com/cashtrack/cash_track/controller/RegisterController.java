37d338b960c6023cf5ec3e61623539c05854eebb
Min Kee Choi <mkc0615@gmail.com>
2022-01-05 21:39:52 +0900

    ADD register controller and service

---
+++ src/main/java/com/cashtrack/cash_track/controller/RegisterController.java
@@ -0,0 +1,25 @@
+package com.cashtrack.cash_track.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RegisterController {
+
+    // Register page
+    @RequestMapping("/register")
+    public String registerPage(){
+        String resultStr = "";
+
+        return resultStr;
+    }
+
+    // Register member
+    @RequestMapping("/join")
+    public void joinMember(@RequestBody String param){
+
+    }
+
+
+}
---
+++ src/main/java/com/cashtrack/cash_track/service/RegisterService.java
@@ -0,0 +1,14 @@
+package com.cashtrack.cash_track.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterService {
+
+    // Join member
+
+    // Modify member
+
+    // Withdraw member
+
+}
