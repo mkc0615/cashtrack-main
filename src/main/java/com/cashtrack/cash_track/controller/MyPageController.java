042d5ae9d2829cc89bc0e54ddd5931d66a7e0d67
Min Kee Choi <mkc0615@gmail.com>
2022-01-16 15:49:37 +0900

    EDIT MyPageController

--- src/main/java/com/cashtrack/cash_track/controller/MyPageController.java
+++ src/main/java/com/cashtrack/cash_track/controller/MyPageController.java
@@ -1,15 +1,30 @@
 package com.cashtrack.cash_track.controller;
 
+import com.cashtrack.cash_track.domain.Account;
+import com.cashtrack.cash_track.domain.MyBond;
+import com.cashtrack.cash_track.domain.Stock;
 import com.cashtrack.cash_track.service.MyPageService;
+import com.cashtrack.cash_track.service.StockService;
+import com.cashtrack.cash_track.service.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class MyPageController {
 
+    @Autowired
     MyPageService myPageService;
+
+    @Autowired
+    TrackService trackService;
+
+    @Autowired
+    StockService stockService;
 
     @RequestMapping("/myPage")
     public String myPage(@RequestBody String param){
@@ -25,6 +40,7 @@ public class MyPageController {
         return resultStr;
     }
 
+    // Account management
     @RequestMapping("/addAccount")
     public String addAccount(){
         String resultStr;
@@ -53,16 +69,29 @@ public class MyPageController {
         return resultStr;
     }
 
-    @GetMapping("/flowInfo")
-    public String getFlowInfo(){
+    @GetMapping("/accounts")
+    public String getAccountList(){
         String resultStr = "";
+
+        List<Account> accountList = myPageService.getAccountList();
 
         return resultStr;
     }
 
-    @GetMapping("/assetInfo")
+    @GetMapping("/stocks")
     public String getAssetInfo(){
         String resultStr = "";
+
+        List<Stock> stockList = stockService.stockList();
+
+        return resultStr;
+    }
+
+    @GetMapping("/tracks")
+    public String getTrackInfo(){
+        String resultStr = "";
+
+        List<MyBond> bondList = trackService.bondList();
 
         return resultStr;
     }
